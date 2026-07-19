7a7633db57ae693704b0fc413eaac8518b9a61ad
elior7467-gif <elior7467@gmail.com>
2026-07-19 03:55:23 +0000

    Update start.go

--- internal/modules/start.go
+++ internal/modules/start.go
@@ -1,8 +1,6 @@
 package modules
 
 import (
-	"time"
-
 	"github.com/Laky-64/gologging"
 	tg "github.com/amarnathcjd/gogram/telegram"
 
@@ -28,6 +26,7 @@ func startHandler(m *tg.NewMessage) error {
 
 	arg := m.Args()
 	database.AddServedUser(m.ChannelID())
+
 	if arg != "" {
 		gologging.Info(
 			"Got Start parameter: " + arg + " in ChatID: " + utils.IntToStr(
@@ -40,32 +39,42 @@ func startHandler(m *tg.NewMessage) error {
 	case "pm_help":
 		gologging.Info("User requested help via start param")
 		helpHandler(m)
+
 	default:
 		caption := F(m.ChannelID(), "start_private", locales.Arg{
 			"user": utils.MentionHTML(m.Sender),
 			"bot":  utils.MentionHTML(m.Client.Me()),
 		})
-		
-		// Using SendOptions with Spoiler field which is globally supported across wrapper versions
-		sendOpt := &tg.SendOptions{
+
+		_, err := m.RespondMedia(&tg.InputMediaWebPage{
+			URL:             config.StartImage,
+			ForceLargeMedia: true,
+		}, &tg.MediaOptions{
 			Caption:     caption,
 			NoForwards:  true,
 			ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
-			Media:       config.StartImage,
-			Spoiler:     true, // This hides the image with a spoiler mesh
-		}
-
-		_, err := m.Respond(caption, sendOpt)
+		})
 		if err != nil {
 			gologging.Error(
-				"[start] Media send with spoiler failed: " + err.Error(),
+				"[start] InputMediaWebPage Reply failed: " + err.Error(),
 			)
-			// Fallback text only if media completely breaks
-			_, err = m.Respond(caption, &tg.SendOptions{
+
+			_, err = m.RespondMedia(config.StartImage, &tg.MediaOptions{
+				Caption:     caption,
 				NoForwards:  true,
 				ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
 			})
-			return err
+			if err != nil {
+				gologging.Error(
+					"[start] URL media reply failed: " + err.Error(),
+				)
+
+				_, err = m.Respond(caption, &tg.SendOptions{
+					NoForwards:  true,
+					ReplyMarkup: core.GetStartMarkup(m.ChannelID()),
+				})
+				return err
+			}
 		}
 	}
 
@@ -86,44 +95,26 @@ func startHandler(m *tg.NewMessage) error {
 			)
 		}
 	}
-
 	return tg.ErrEndGroup
 }
 
 func startCB(cb *tg.CallbackQuery) error {
 	cb.Answer("")
+
 	caption := F(cb.ChannelID(), "start_private", locales.Arg{
 		"user": utils.MentionHTML(cb.Sender),
 		"bot":  utils.MentionHTML(cb.Client.Me()),
 	})
+
 	sendOpt := &tg.SendOptions{
 		ReplyMarkup: core.GetStartMarkup(cb.ChannelID()),
 		NoForwards:  true,
 	}
+
 	if config.StartImage != "" {
 		sendOpt.Media = config.StartImage
 	}
-	cb.Edit(caption, sendOpt)
-	return tg.ErrEndGroup
-}
 
-func aboutCB(cb *tg.CallbackQuery) error {
-	cb.Answer("")
-
-	uptime := time.Since(config.StartTime).Round(time.Second)
-
-	caption := F(cb.ChannelID(), "about_text", locales.Arg{
-		"bot":    utils.MentionHTML(cb.Client.Me()),
-		"uptime": uptime.String(),
-	})
-
-	sendOpt := &tg.SendOptions{
-		ReplyMarkup: core.GetBackToStartKeyboard(cb.ChannelID()),
-		NoForwards:  true,
-	}
-	if config.StartImage != "" {
-		sendOpt.Media = config.StartImage
-	}
 	cb.Edit(caption, sendOpt)
 	return tg.ErrEndGroup
 }
